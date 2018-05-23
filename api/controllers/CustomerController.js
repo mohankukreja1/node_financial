0621175937d04d07743e1a9bffbe66de8a8ae83c
mohan <mohankukreja1@gmail.com>
2018-05-23 12:57:59 +0530

    backend with sails created
    Signed-off-by: mohan <mohankukreja1@gmail.com>

---
+++ api/controllers/CustomerController.js
@@ -0,0 +1,32 @@
+/**
+ * CustomerController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  'new': function (req ,res) {
+    res.view('customer/new')
+  },
+  create:function (req,res,next) {
+
+    Customer.create(req.query, function (err ,customer) {
+if(err) return next(err);
+res.redirect('/customer/show')
+
+    })
+
+  },
+  show:function (req,res,next) {
+    Customer.find(function (err,response) {
+      if(err) throw err;
+      res.view('customer/show',{
+        customer:response
+      })
+    })
+  }
+
+
+};
+
